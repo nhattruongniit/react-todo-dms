034fad3d52a2e10535b8c3fa509afcdcd5cb6024
TonyNguyen <truong.nn@amanotes.com>
2022-06-24 09:22:09 +0700

    add button todo

--- src/views/Todo/Todo.tsx
+++ src/views/Todo/Todo.tsx
@@ -1,76 +1,96 @@
-import React from 'react';
+import React from "react";
 
 // mui core
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
-import Button from '@mui/material/Button';
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 // components
-import { TableNutrition } from 'components/Table';
-import { FabAdd } from 'components/Fab';
+import { TableNutrition } from "components/Table";
+import { FabAdd } from "components/Fab";
 
 // context
-import { useTodoContext } from 'context/TodoContext';
+import { useTodoContext } from "context/TodoContext";
 
 // sections
-import AddEditTodo from './components/AddEditTodo';
-import DeleteTodo from './components/DeleteTodo';
+import AddEditTodo from "./components/AddEditTodo";
+import DeleteTodo from "./components/DeleteTodo";
 
 // model
-import{ IHeadCell } from 'models/ITable';
-import { ITodoItem } from 'models/ITodo';
+import { IHeadCell } from "models/ITable";
+import { ITodoItem } from "models/ITodo";
 
 const headCells: IHeadCell[] = [
   {
-    id: 'title',
+    id: "title",
     numeric: false,
-    label: 'Title',
-    dataTestId: 'title'
+    label: "Title",
+    dataTestId: "title",
   },
   {
-    id: 'status',
+    id: "status",
     numeric: false,
-    label: 'Status',
-    dataTestId: 'status'
+    label: "Status",
+    dataTestId: "status",
   },
   {
-    id: 'actions',
+    id: "actions",
     numeric: false,
-    label: 'Actions',
-    dataTestId: 'actions'
-  }
+    label: "Actions",
+    dataTestId: "actions",
+  },
 ];
 
 function Todo() {
-  const { todos, setTodoItem, handleOpenModal, handleDeleteTodo } = useTodoContext();
+  const { todos, setTodoItem, handleOpenModal, handleDeleteTodo } =
+    useTodoContext();
 
   // edit todo
   const handleEditTodo = (rowItem: ITodoItem) => () => {
-    handleOpenModal('edit');
+    handleOpenModal("edit");
     setTodoItem(rowItem);
-  }
+  };
 
   return (
     <>
-      <TableNutrition 
+      <Grid
+        container
+        item
+        sx={{ display: "flex", justifyContent: "flex-end", mb: 2 }}
+      >
+        <Button variant="contained" onClick={() => handleOpenModal("add")}>
+          Add Todo
+        </Button>
+      </Grid>
+
+      <TableNutrition
         headCells={headCells}
         rows={todos}
         renderRows={(rowItem: ITodoItem) => (
-          <TableRow
-            hover
-            tabIndex={-1}
-            key={rowItem.id}
-          >
-            <TableCell align="left" data-testid="title">{rowItem.title}</TableCell>
-            <TableCell 
+          <TableRow hover tabIndex={-1} key={rowItem.id}>
+            <TableCell align="left" data-testid="title">
+              {rowItem.title}
+            </TableCell>
+            <TableCell
               align="left"
-              sx={{ color: rowItem.completed ? "#4caf50" : "rgba(0, 0, 0, 0.87)" }}
+              sx={{
+                color: rowItem.completed ? "#4caf50" : "rgba(0, 0, 0, 0.87)",
+              }}
             >
               {rowItem.completed ? "Completed" : "New"}
             </TableCell>
             <TableCell align="left">
-              <Button variant="text" onClick={handleEditTodo(rowItem)}>Edit</Button>  
-              <Button variant="text" color='error' onClick={handleDeleteTodo(rowItem)}>Delete</Button>  
+              <Button variant="text" onClick={handleEditTodo(rowItem)}>
+                Edit
+              </Button>
+              <Button
+                variant="text"
+                color="error"
+                onClick={handleDeleteTodo(rowItem)}
+              >
+                Delete
+              </Button>
             </TableCell>
           </TableRow>
         )}
@@ -80,14 +100,9 @@ function Todo() {
       <DeleteTodo />
 
       {/* section dialog add todo */}
-      <AddEditTodo  />
-
-      {/* section button add */}
-      <FabAdd 
-        onClick={() => handleOpenModal('add')}
-      />
+      <AddEditTodo />
     </>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
--- src/views/Todo/__tests__/Todo.test.tsx
+++ src/views/Todo/__tests__/Todo.test.tsx
@@ -1,25 +1,28 @@
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import { rest } from "msw";
-import { setupServer } from 'msw/node'
+import { setupServer } from "msw/node";
 
 // components
-import Todo from 'views/Todo';
+import Todo from "views/Todo";
 
 // context
-import { TodoProvider } from 'context/TodoContext';
+import { TodoProvider } from "context/TodoContext";
 
-const todoResponse: any = rest.get(`https://jsonplaceholder.typicode.com/todos`, (req, res, ctx) => {
-  return res(
-    ctx.json([
-      {
-        completed: false,
-        id: 1,
-        title: "delectus aut autem",
-        userId: 1,
-      }
-    ])
-  )
-})
+const todoResponse: any = rest.get(
+  `https://jsonplaceholder.typicode.com/todos`,
+  (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          completed: false,
+          id: 1,
+          title: "delectus aut autem",
+          userId: 1,
+        },
+      ])
+    );
+  }
+);
 
 const handlers = [todoResponse];
 const server = setupServer(...handlers);
@@ -28,22 +31,22 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('should fetch todo success when first render', async () => {
+test("should fetch todo success when first render", async () => {
   // given
   render(
     <TodoProvider>
-     <Todo />
+      <Todo />
     </TodoProvider>
-   );
+  );
 
-   // when
+  // when
   const todoItem = await screen.findByText("delectus aut autem");
 
   // then
   expect(todoItem).toBeVisible();
 });
 
-test('should render column Title', async () => {
+test("should render column Title", async () => {
   // given
   render(<Todo />);
 
@@ -54,8 +57,7 @@ test('should render column Title', async () => {
   expect(col).toBeVisible();
 });
 
-
-test('should render column Status', async () => {
+test("should render column Status", async () => {
   // given
   render(<Todo />);
 
@@ -66,8 +68,7 @@ test('should render column Status', async () => {
   expect(col).toBeVisible();
 });
 
-
-test('should render column Actions', async () => {
+test("should render column Actions", async () => {
   // given
   render(<Todo />);
 
